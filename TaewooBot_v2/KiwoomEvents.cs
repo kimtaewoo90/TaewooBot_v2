e0f4b23869114b6864f668377a0f9bc26e82a419
kimtaewoo90 <52480205+kimtaewoo90@users.noreply.github.com>
2021-07-20 22:45:27 +0900

    Add files via upload

---
+++ TaewooBot_v2/KiwoomEvents.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TaewooBot_v2
+{
+    public partial class Main
+    {
+
+        // KiwoomEvents
+        List<ConditionInfo> conditionList;
+
+        // Send Condition
+        private void OnReceiveConditionVer(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveConditionVerEvent e)
+        {
+            conditionList = new List<ConditionInfo>();
+
+            string condtionNameList = API.GetConditionNameList();
+            string[] conditionNameArray = condtionNameList.Split(';');
+
+            for (int i = 0; i < conditionNameArray.Length; i++)
+            {
+                string[] ConditionInfo = conditionNameArray[i].Split('^');
+                if (ConditionInfo.Length == 2)
+                {
+                    conditionList.Add(new ConditionInfo()
+                    {
+                        ConditionNum = int.Parse(ConditionInfo[0].Trim()),
+                        ConditionNm = ConditionInfo[1].Trim()
+                    });
+                }
+            }
+
+            API.SendCondition(get_scr_no(), conditionList[0].ConditionNm, conditionList[0].ConditionNum, 1);
+
+
+        }
+
+        // Get Condition
+        private void OnReceiveTrCondition(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrConditionEvent e)
+        {
+            if (e.strCodeList.Length > 0)
+            {
+                string stockCodeList = e.strCodeList.Remove(e.strCodeList.Length - 1);
+                int stockCount = stockCodeList.Split(';').Length;
+
+                if (stockCount <= 100)
+                {
+                    //g_rqname = "조건검색종목";
+                    API.CommKwRqData(stockCodeList, 0, stockCount, 0, "조건검색종목", "5100");
+                }
+            }
+            else if (e.strCodeList.Length == 0)
+            {
+                write_sys_log("검색된 조건목록에 대한 종목이 없습니다.\n", 0);
+            }
+        }
+        private void OnReceiveRealCondition(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveRealConditionEvent e)
+        {
+            conditionList = new List<ConditionInfo>();
+
+            //종목 편입
+            if (e.strType.Equals("I"))
+            {
+                string stockName = API.GetMasterCodeName(e.sTrCode);
+                write_sys_log("편입종목 : " + "[" + stockName + " ].\n", 0);
+
+                string scr_no = null;
+                scr_no = "";
+                scr_no = get_scr_no();
+
+                string msg = "편입종목 : " + stockName;
+
+                write_sys_log(msg, 0);
+
+                // TargetStocks DataGridView 추가
+                DisplayTargetStocks("Insert", e.sTrCode.ToString(), stockName, "0", "0", "0");
+
+                // 추가된 종목만 따로 실시간 요청
+                ReqRealData(e.sTrCode, scr_no);
+            }
+
+            //종목 이탈
+            else if (e.strType.Equals("D"))
+            {
+                string stockName = API.GetMasterCodeName(e.sTrCode);
+                write_sys_log("이탈종목 : " + "[" + stockName + " ].\n", 0);
+
+                string scr_no = targetDict[e.sTrCode];
+                API.DisconnectRealData(scr_no);
+
+                int i = 0;
+                for (i = 0; i < TargetCodes.Count; i++)
+                {
+                    if (TargetCodes[i].Equals(e.sTrCode))
+                    {
+                        TargetCodes.RemoveAt(i);
+                        break;
+                    }
+                }
+
+                TargetCodes.Remove(e.sTrCode);
+
+                // Remove 이탈Stock in Dictionary
+                RemoveDict(e.sTrCode);
+
+                write_sys_log("이탈종목 : " + stockName, 0);
+
+                DeleteTargetStocks(e.sTrCode.ToString());
+            }
+
+            write_sys_log(TargetCodes.ToString(), 0);
+
+        }
+
+        // Get TR Data
+        private void OnReceiveTrData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+        {
+            if (RqName.CompareTo(e.sRQName) != 0)
+            {
+                write_sys_log("Should check the TRCode", 0);
+            }
+            else
+            {
+                switch (RqName)
+                {
+                    // OPW00001
+                    case "예수금상세현황요청":
+                        write_sys_log("예수금상세현황 요청", 0);
+                        break;
+
+                    // OPW00004
+                    case "계좌평가현황요청":
+                        write_sys_log("계좌평가현황 요청", 0);
+                        break;
+
+                    case "호가조회":
+                        write_sys_log("호가조회 요청", 0);
+                        break;
+
+                    case "현재가조회":
+                        write_sys_log("현재가조회 요청", 0);
+                        break;
+
+                    case "주식시세":  // for 거래량 조회
+                        write_sys_log("주식시세 요청", 0);
+                        
+                        break;
+
+                    case "주식기본정보":  // for 주식기본정보
+                        write_sys_log("주식기본정보 요청", 0);
+                        break;
+
+
+                    default: break;
+                }
+            }
+
+            if (e.sRQName == "현재가조회")
+            {
+                 API.CommGetData(e.sTrCode, "", e.sRQName, 0, "현재가");
+
+            }
+
+            if (e.sRQName == "주식기본정보")
+            {
+                string Code = API.GetCommData(e.sTrCode, e.sRQName, 0, "종목코드").Trim().ToString();
+                string KrName = API.GetCommData(e.sTrCode, e.sRQName, 0, "종목명").Trim().ToString();
+                string Price = API.GetCommData(e.sTrCode, e.sRQName, 0, "현재가").Trim().ToString();
+
+
+                TargetCodes.Add(Code);
+                StockKrNameDict.Add(Code, KrName);
+                StockPriceDict.Add(Code, Price);
+
+
+                // StockInfo class의 instance가 동적으로 안되면 각 항목마다 DIct를 줘서 비교.
+
+                // TODO : Save the Stock Data at Dictionary in here.
+             
+                write_sys_log("주식기본정보 받기 성공", 0);
+                
+            }
+
+            if (e.sRQName == "조건검색종목")
+            {
+                int count = API.GetRepeatCnt(e.sTrCode, e.sRQName); //요청의 반복 횟수를 요청합니다.
+                write_sys_log("조건검색종목 요청", 0);
+
+                for (int i = 0; i < count; i++)
+                {
+                    string stockCode = API.GetCommData(e.sTrCode, e.sRQName, i, "종목코드").Trim();
+                    string stockName = API.GetCommData(e.sTrCode, e.sRQName, i, "종목명").Trim();
+                    double currentPrice = double.Parse(API.GetCommData(e.sTrCode, e.sRQName, i, "현재가").Replace("-", ""));
+                    double upDownRate = double.Parse(API.GetCommData(e.sTrCode, e.sRQName, i, "등락율"));
+                    int netChange = int.Parse(API.GetCommData(e.sTrCode, e.sRQName, i, "전일대비"));
+                    long volume = long.Parse(API.GetCommData(e.sTrCode, e.sRQName, i, "거래량"));                
+                }
+            }
+
+            if (e.sRQName == "예수금상세현황요청")
+            {
+                Deposit = Double.Parse(API.GetCommData(e.sTrCode, e.sRQName, 0, "예수금").Trim());
+                write_sys_log(Deposit.ToString(), 0);
+            }
+
+            if (e.sRQName == "계좌평가현황요청")
+            {
+                Deposit = Double.Parse(API.GetCommData(e.sTrCode, e.sRQName, 0, "예수금").Trim());
+
+                AccountStockLots = API.GetRepeatCnt(e.sTrCode, e.sRQName);
+                for (int nIdx = 0; nIdx < AccountStockLots; nIdx++)
+                {
+                    string code = API.GetCommData(e.sTrCode, e.sRQName, AccountStockLots, "종목코드").Trim();
+                    Accnt_StockName[code] = API.GetCommData(e.sTrCode, e.sRQName, AccountStockLots, "종목명").Trim();
+                    Accnt_StockLots[code] = API.GetCommData(e.sTrCode, e.sRQName, AccountStockLots, "보유수량").Trim();
+                    Accnt_StockPnL[code] = API.GetCommData(e.sTrCode, e.sRQName, AccountStockLots, "손익율").Trim();
+                    Accnt_StockPnL_Won[code] = API.GetCommData(e.sTrCode, e.sRQName, AccountStockLots, "손익금액").Trim();
+
+                    string msg = $"종목명 : {Accnt_StockName[code]}, 보유수량 : {Accnt_StockLots[code]}, 손익율 : {Accnt_StockLots[code]}, 손익금액 : {Accnt_StockPnL_Won}";
+                    write_sys_log(msg, 0);
+                }
+
+                if (AccountStockLots == 0)
+                {
+                    write_sys_log("보유종목이 없습니다.", 0);
+                }
+            }
+        }
+
+        // Get Real Data
+        private void OnReceiveRealData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveRealDataEvent e)
+        {
+
+            if (e.sRealType == "주식시세")
+            {
+                int Volume = 0;
+                Volume = int.Parse(API.GetCommRealData(e.sRealType, 13)); // 누적거래량
+
+                string Code = e.sRealKey;
+                string KrName = GetKrName(Code);
+                double buy_amt;
+                double Price = double.Parse(API.GetCommRealData(e.sRealType, 10).Trim());  // current price;
+                double UpDownRate = double.Parse(API.GetCommRealData(e.sRealType, 12));
+                string ContractLots = API.GetCommRealData(e.sRealType, 15).Trim().ToString(); // 체결량
+                double buy_price = get_hoga_unit_price((int)Price, Code, -2);
+ 
+                _GetRTD = true;
+
+                // Update Dictionary
+                StockPriceDict[Code] = Price.ToString();
+                StockPnLDict[Code] = UpDownRate.ToString();
+                StockKrNameDict[Code] = KrName;
+
+                // ToDo : GetTickSpeed in here
+                GetTickSpeed(Code, ContractLots);
+
+                // Display the RTD data on TargetStocks DataGridView
+                DisplayTargetStocks("Update", Code, "", Price.ToString(), ContractLots, UpDownRate.ToString());
+
+                // Logs
+                write_sys_log("RTD for " + KrName + " : " + Price.ToString(), 0);
+
+            }
+
+
+            if (e.sRealType == "주식체결")
+            {
+                // 체결 시 들어옴.
+            }
+
+
+        }
+    }
+}
---
+++ TaewooBot_v2/Main.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace TaewooBot_v2
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.MarketType = new System.Windows.Forms.ComboBox();
+            this.Start_Btn = new System.Windows.Forms.Button();
+            this.Log = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.API = new AxKHOpenAPILib.AxKHOpenAPI();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.Status = new System.Windows.Forms.ToolStripStatusLabel();
+            this.CurrentTime = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.TargetStocks = new System.Windows.Forms.DataGridView();
+            this.StockCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StockKrName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TickSpeed = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PnL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TestBtn = new System.Windows.Forms.Button();
+            this.TestText = new System.Windows.Forms.TextBox();
+            this.DisplayBtn = new System.Windows.Forms.Button();
+            this.DelBtn = new System.Windows.Forms.Button();
+            this.GetDeposit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.API)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TargetStocks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MarketType
+            // 
+            this.MarketType.BackColor = System.Drawing.SystemColors.Info;
+            this.MarketType.FormattingEnabled = true;
+            this.MarketType.Items.AddRange(new object[] {
+            "Stock",
+            "Coin"});
+            this.MarketType.Location = new System.Drawing.Point(24, 12);
+            this.MarketType.Name = "MarketType";
+            this.MarketType.Size = new System.Drawing.Size(121, 20);
+            this.MarketType.TabIndex = 3;
+            this.MarketType.SelectedIndexChanged += new System.EventHandler(this.MarketType_SelectedIndexChanged);
+            // 
+            // Start_Btn
+            // 
+            this.Start_Btn.Location = new System.Drawing.Point(163, 9);
+            this.Start_Btn.Name = "Start_Btn";
+            this.Start_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Start_Btn.TabIndex = 4;
+            this.Start_Btn.Text = "Start";
+            this.Start_Btn.UseVisualStyleBackColor = true;
+            this.Start_Btn.Click += new System.EventHandler(this.Start_Btn_Click);
+            // 
+            // Log
+            // 
+            this.Log.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Log.Font = new System.Drawing.Font("굴림", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Log.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Log.Location = new System.Drawing.Point(6, 20);
+            this.Log.Multiline = true;
+            this.Log.Name = "Log";
+            this.Log.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.Log.Size = new System.Drawing.Size(764, 360);
+            this.Log.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Log);
+            this.groupBox1.Location = new System.Drawing.Point(12, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 386);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Logs";
+            // 
+            // API
+            // 
+            this.API.Enabled = true;
+            this.API.Location = new System.Drawing.Point(699, -6);
+            this.API.Name = "API";
+            this.API.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("API.OcxState")));
+            this.API.Size = new System.Drawing.Size(100, 50);
+            this.API.TabIndex = 7;
+            this.API.Visible = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Status,
+            this.CurrentTime});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 517);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1377, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "time";
+            // 
+            // Status
+            // 
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(135, 17);
+            this.Status.Text = "Welcome to TaewooBot";
+            // 
+            // CurrentTime
+            // 
+            this.CurrentTime.Name = "CurrentTime";
+            this.CurrentTime.Size = new System.Drawing.Size(54, 17);
+            this.CurrentTime.Text = "time line";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.TargetStocks);
+            this.groupBox2.Location = new System.Drawing.Point(810, 39);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(541, 386);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Target Stocks";
+            // 
+            // TargetStocks
+            // 
+            this.TargetStocks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TargetStocks.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.StockCode,
+            this.StockKrName,
+            this.Price,
+            this.TickSpeed,
+            this.PnL});
+            this.TargetStocks.Location = new System.Drawing.Point(7, 21);
+            this.TargetStocks.Name = "TargetStocks";
+            this.TargetStocks.RowTemplate.Height = 23;
+            this.TargetStocks.Size = new System.Drawing.Size(528, 359);
+            this.TargetStocks.TabIndex = 0;
+            // 
+            // StockCode
+            // 
+            this.StockCode.HeaderText = "종목코드";
+            this.StockCode.Name = "StockCode";
+            // 
+            // StockKrName
+            // 
+            this.StockKrName.HeaderText = "종목명";
+            this.StockKrName.Name = "StockKrName";
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "현재가";
+            this.Price.Name = "Price";
+            // 
+            // TickSpeed
+            // 
+            this.TickSpeed.HeaderText = "틱속도";
+            this.TickSpeed.Name = "TickSpeed";
+            // 
+            // PnL
+            // 
+            this.PnL.HeaderText = "수익률";
+            this.PnL.Name = "PnL";
+            // 
+            // TestBtn
+            // 
+            this.TestBtn.Location = new System.Drawing.Point(248, 479);
+            this.TestBtn.Name = "TestBtn";
+            this.TestBtn.Size = new System.Drawing.Size(75, 23);
+            this.TestBtn.TabIndex = 10;
+            this.TestBtn.Text = "Test";
+            this.TestBtn.UseVisualStyleBackColor = true;
+            this.TestBtn.Click += new System.EventHandler(this.TestBtn_Click);
+            // 
+            // TestText
+            // 
+            this.TestText.Location = new System.Drawing.Point(104, 479);
+            this.TestText.Name = "TestText";
+            this.TestText.Size = new System.Drawing.Size(138, 21);
+            this.TestText.TabIndex = 11;
+            // 
+            // DisplayBtn
+            // 
+            this.DisplayBtn.Location = new System.Drawing.Point(365, 479);
+            this.DisplayBtn.Name = "DisplayBtn";
+            this.DisplayBtn.Size = new System.Drawing.Size(75, 23);
+            this.DisplayBtn.TabIndex = 12;
+            this.DisplayBtn.Text = "Display";
+            this.DisplayBtn.UseVisualStyleBackColor = true;
+            this.DisplayBtn.Click += new System.EventHandler(this.DisplayBtn_Click);
+            // 
+            // DelBtn
+            // 
+            this.DelBtn.Location = new System.Drawing.Point(446, 479);
+            this.DelBtn.Name = "DelBtn";
+            this.DelBtn.Size = new System.Drawing.Size(75, 23);
+            this.DelBtn.TabIndex = 13;
+            this.DelBtn.Text = "Delete";
+            this.DelBtn.UseVisualStyleBackColor = true;
+            this.DelBtn.Click += new System.EventHandler(this.DelBtn_Click);
+            // 
+            // GetDeposit
+            // 
+            this.GetDeposit.Location = new System.Drawing.Point(551, 478);
+            this.GetDeposit.Name = "GetDeposit";
+            this.GetDeposit.Size = new System.Drawing.Size(75, 23);
+            this.GetDeposit.TabIndex = 14;
+            this.GetDeposit.Text = "Deposit";
+            this.GetDeposit.UseVisualStyleBackColor = true;
+            this.GetDeposit.Click += new System.EventHandler(this.GetDeposit_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1377, 539);
+            this.Controls.Add(this.GetDeposit);
+            this.Controls.Add(this.DelBtn);
+            this.Controls.Add(this.DisplayBtn);
+            this.Controls.Add(this.TestText);
+            this.Controls.Add(this.TestBtn);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.API);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Start_Btn);
+            this.Controls.Add(this.MarketType);
+            this.Name = "Main";
+            this.Text = "TaewooBot";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.API)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.TargetStocks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox MarketType;
+        private System.Windows.Forms.Button Start_Btn;
+        private System.Windows.Forms.TextBox Log;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private AxKHOpenAPILib.AxKHOpenAPI API;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel Status;
+        private System.Windows.Forms.ToolStripStatusLabel CurrentTime;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView TargetStocks;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StockCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StockKrName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TickSpeed;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PnL;
+        private System.Windows.Forms.Button TestBtn;
+        private System.Windows.Forms.TextBox TestText;
+        private System.Windows.Forms.Button DisplayBtn;
+        private System.Windows.Forms.Button DelBtn;
+        private System.Windows.Forms.Button GetDeposit;
+    }
+}
+
